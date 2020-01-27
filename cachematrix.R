598c941efcbe448a4693520d60395331a32f8df6
greycells2008 <archanam2008@gmail.com>
2020-01-27 15:10:33 +0530

    The functions for matrix inverse caching and checking were added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used to cache the inverse of a matrix which would otherwise
+## be a very costly and time-consuming operation
 
-## Write a short comment describing this function
+## This function is used to cache the inverse and the original matrix for 
+## repeated use
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  storedMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse){
+    inv <<- inverse
+    storedMatrix <<- x
+  }
+  getinverse <- function() inv
+  getstoredmatrix <- function() storedMatrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse,
+       getstoredmatrix=getstoredmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function is used to check if cached inverse exists and the matrix is unchanged
+##  else it recalculates the inverse of the given matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    if((nrow(x$getstoredmatrix())==nrow(x$get())) && 
+       (ncol(x$getstoredmatrix())==ncol(x$get())))
+    {
+      if(identical(x$getstoredmatrix(),x$get()))
+      {
+        message("getting cached data")
+        return(inv)
+      }
+    }
+    
+  }
+  data <- x$get()
+  newInv <- solve(data)
+  x$setinverse(newInv)
+  newInv
 }
